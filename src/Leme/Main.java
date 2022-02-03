4f20d033f192d6d29a00644c141c890b86acc28a
Elbon Eastmage <ejeast3@gmail.com>
2022-02-02 20:14:20 -0700

    Added a friendly message

--- src/Leme/Main.java
+++ src/Leme/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.print("Hello, my name is Lorenzo");
+        System.out.println("\nHello Lorenzo, my name is Elbert.");
     }
 }
